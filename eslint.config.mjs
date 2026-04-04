a37af04c10a3715620c8cc025af10325a2cbdf84
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-04-03 21:16:38 -0300

    chore: add next js setup

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
+    plugins: ["simple-import-sort"],
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+    parserOptions: {
+      sourceType: "module",
+      ecmaVersion: "latest",
+    },
+  },
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
+
+export default eslintConfig;
