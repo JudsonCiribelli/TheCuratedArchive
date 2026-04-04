9be6e518270fbaccafecfa888c8f4063b09cd1ae
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-04-03 22:02:06 -0300

    chore: add poppins font on project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,13 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+import { cn } from "@/app/lib/utils";
+
+const poppins = Poppins({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -26,11 +21,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className={cn("h-full", "antialiased", poppins.className)}>
+      <body className="flex min-h-full flex-col">{children}</body>
     </html>
   );
 }
