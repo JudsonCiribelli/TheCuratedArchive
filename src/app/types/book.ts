eecfef5f1f2e2c7b91eaaf9aa9e78ec39169271e
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-04-07 11:14:41 -0300

    feat: add books type

---
+++ src/app/types/book.ts
@@ -0,0 +1,11 @@
+export interface BookTypes {
+  id: string;
+  title: string;
+  description: string;
+  pages: number;
+  year: number;
+  bookImage: string;
+  status: string;
+  authorId: string;
+  categoryId: string;
+}
