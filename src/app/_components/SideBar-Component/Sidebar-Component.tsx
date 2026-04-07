22a68ad13126037689bcafec5e01ac8e87ef212f
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-04-07 11:19:08 -0300

    feat: add side bar component

---
+++ src/app/_components/SideBar-Component/Sidebar-Component.tsx
@@ -0,0 +1,29 @@
+import { BookOpen, BookOpenText } from "lucide-react";
+
+import { categoryType } from "@/app/types/category";
+
+import SideBarButton from "../Sidebar-Button/Sidebar-Button";
+
+const SidebarComponent = ({ categories }: { categories: categoryType[] }) => {
+  return (
+    <div className="h-screen w-64 bg-[#ececf1]">
+      <div className="flex items-center gap-2 px-8 py-6">
+        <BookOpen size={25} className="text-[#0a3968]" />
+        <div className="flex flex-col">
+          <h2 className="text-2xl font-semibold text-[#0a3968]">Categorias</h2>
+          <p className="text-xs text-slate-400">NAVEGUE POR GÊNERO</p>
+        </div>
+      </div>
+      <div className="flex flex-col gap-1 p-2">
+        {categories.map((categorie) => (
+          <SideBarButton href="/" key={categorie.id}>
+            <BookOpenText size={10} />
+            {categorie.name}
+          </SideBarButton>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SidebarComponent;
