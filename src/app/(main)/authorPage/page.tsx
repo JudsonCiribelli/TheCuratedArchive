8446f93c2c6685509494e3c34ccf0d9870f1e18f
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-04-13 19:33:07 -0300

    feat: add authors page

---
+++ src/app/(main)/authorPage/page.tsx
@@ -0,0 +1,29 @@
+import { getAllAuthors } from "@/app/actions/get-all-authors";
+
+import AuthorCard from "./_components/Author-Card-Component/Author-Card";
+import AuthorData from "./_components/Author-Data-Component/Author-Data";
+
+const AuthorPage = async () => {
+  const authors = await getAllAuthors();
+
+  return (
+    <div className="flex flex-col">
+      <AuthorCard authorCount={authors.length} />
+
+      <div className="flex flex-col gap-4 xl:flex-row">
+        {authors.map((author) => (
+          <AuthorData
+            id={author.id}
+            key={author.id}
+            name={author.name}
+            bio={author.bio}
+            authorImage={author.authorImage}
+            quantityBook={author.books.length}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AuthorPage;
