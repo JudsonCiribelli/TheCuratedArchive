e5af655c7f561a315168d24ddda2a2d021fdb322
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-04-14 19:25:11 -0300

    refactor: all books actions in just one file

--- src/app/actions/get-books.ts
+++ src/app/actions/get-books.ts
@@ -1,5 +1,6 @@
 "use server";
 import { apiClient } from "../lib/api";
+import { AuthorBooksType } from "../types/authorBook";
 import { BookTypes } from "../types/book";
 
 export async function getBooks() {
@@ -12,5 +13,33 @@ export async function getBooks() {
   } catch (error) {
     console.log(error);
     return [];
+  }
+}
+
+export async function getBookById(id: string): Promise<BookTypes> {
+  try {
+    const response = await apiClient<BookTypes>(`/books/${id}`, {
+      method: "GET",
+    });
+
+    return response;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error ao buscar dados do livro");
+  }
+}
+
+export async function getBookByAuthorId(
+  id: string,
+): Promise<AuthorBooksType[]> {
+  try {
+    const response = await apiClient<AuthorBooksType[]>(`/books/author/${id}`, {
+      method: "GET",
+    });
+
+    return response;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error ao buscar livro deste autor.");
   }
 }
