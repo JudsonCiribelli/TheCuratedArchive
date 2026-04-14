ea5f5bcbc46844a1ef0d10adee24aea917269ff1
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-04-14 19:25:34 -0300

    refactor: all categories actions in just one file

--- src/app/actions/get-categories.ts
+++ src/app/actions/get-categories.ts
@@ -1,5 +1,6 @@
 "use server";
 import { apiClient } from "../lib/api";
+import { BookByCategory } from "../types/booksByCategory";
 import { categoryType } from "../types/category";
 
 export async function getCategory() {
@@ -12,5 +13,38 @@ export async function getCategory() {
   } catch (error) {
     console.log(error);
     throw new Error("Error ao buscar categoria");
+  }
+}
+
+export async function getCategoryData(
+  categoryId: string,
+): Promise<categoryType[]> {
+  try {
+    const data = await apiClient<categoryType[]>(`/category/${categoryId}`, {
+      method: "GET",
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error ao buscar categoria");
+  }
+}
+
+export async function getBookByCategoryId(
+  categoryId: string,
+): Promise<BookByCategory[]> {
+  try {
+    const response = await apiClient<BookByCategory[]>(
+      `/books/category/${categoryId}`,
+      {
+        method: "GET",
+      },
+    );
+
+    return response;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error ao buscar livros");
   }
 }
